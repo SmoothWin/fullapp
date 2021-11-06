606d48f014697e27265e60d3c97ed71ba4e7e49f
Christian Chitanu <christian.chitanu@hotmail.com>
2021-11-06 00:45:40 -0400

    added more test features

---
+++ backend/dbConnection.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const mysql = require('mysql')
+const con = mysql.createConnection({
+    host:process.env.DB_HOST,
+    port:process.env.DB_PORT,
+    user:process.env.DB_USER,
+    password:process.env.DB_SECRET,
+    database:process.env.DB_DB
+})
+
+module.exports = con;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
 require('dotenv').config()
+const mysqlCon = require('./dbConnection')
 const express = require('express');
 const app = express()
 const port = process.env.PORT || 3000;
 
 
+
+app.use('/',(req,res,next)=>{
+    mysqlCon.connect(function(err){
+        if(err) throw err;
+        console.log(`Connected to sql DB`)
+    });
+    next()
+})
+
 app.get('/',(req,res)=>{
-    res.send("Hello")
+    
+    let list = null;
+    mysqlCon.query({
+        sql:'select * from user',
+        timeout: 10000
+    }, (err, result, fields)=>{
+        if(err) throw err;
+        list = result;
+    })
+    mysqlCon.end(()=>{
+        console.log(`Closed connection to sql DB`)
+    })
+    res.send(list);
 })
 
 app.listen(port, ()=>{
